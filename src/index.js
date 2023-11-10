ee055d875a5c4ede943b656a8c32c28981ce6417
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-11-09 23:46:00 -0300

    fix:ProxyError

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors")
 
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-app.get("/", async (req, res) => {
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // You can specify the allowed origins here
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true,
+  optionSuccessStatus: 200,
+}
+
+app.enable("trust proxy");
+
+app.get("/", async (req, res,) => {
     try{
     const response = await axios.get('https://api.cartola.globo.com/partidas');
     const data = response.data;
@@ -16,10 +32,10 @@ app.get("/", async (req, res) => {
 }
 });
 
-// app.listen(port, () => {
-//     console.log(`Servidor Rodando na porta ${port}`);
-// })
+app.listen(port, () => {
+    console.log(`Servidor Rodando na porta ${port}`);
+})
 
-const server = app.listen(3000);
+// const server = app.listen(3000);
 
 module.exports = { app };
